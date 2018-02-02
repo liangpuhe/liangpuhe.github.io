3924ebabcc3919b855df18cb3db43e82ced73fdd
liangpuhe <245738112@qq.com>
2018-02-01 23:46:31 -0500

    upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+ // import inView from './in-view.min.js';
+
+//
+inView('.blog-header-container')
+    .on('exit', function(){document.getElementById("navDots").style.display = 'inline-block'})
+    .on('enter', function(){document.getElementById("navDots").style.display = 'none'});
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,14 +20,14 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" dot-active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " dot-active";
 }
