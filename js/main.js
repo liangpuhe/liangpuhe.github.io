ed6ca6a460d39c872fc924f88321ba0dd2fafebd
liangpuhe <245738112@qq.com>
2018-12-11 13:42:39 -0500

    upload

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@
    }
  });
  $('#return-to-top').click(function() { // When arrow is clicked
-   $('body,html').animate({
+   $('body.html').animate({
      scrollTop: 0 // Scroll to top of body
    }, 500);
  });
